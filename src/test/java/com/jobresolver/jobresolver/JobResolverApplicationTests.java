e3b181e33bcc72f2a3e9bc168b5aa3e32b138bb8
naskodobrev <atanas.vasilev.dobrev@gmail.com>
2021-11-05 17:02:12 +0200

    [finish] front-end.

--- src/test/java/com/jobresolver/jobresolver/JobResolverApplicationTests.java
+++ src/test/java/com/jobresolver/jobresolver/JobResolverApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jobresolver.jobresolver;
 
+import lombok.var;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class JobResolverApplicationTests {
 
+    @InjectMocks
+    @Resource
+    JobResolver jobResolver;
+
 	@Test
-	void contextLoads() {
+	void shouldJobOpportunitiesBeMatchedCorrectly() {
+        var answer = jobResolver.matchJobOpportunities(new ArrayList<>(), new ArrayList<>());
+
+        assertEquals(answer, new ArrayList<>());
 	}
 
 }
